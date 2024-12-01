9a2d1a06f700a2c3def0a32571f2433d84748c86
Shashank Lokare <lokaress@gmail.com>
2024-12-01 15:51:07 +0100

    new karate basic project

--- src/test/java/examples/tests/TestsRunner.java
+++ src/test/java/examples/tests/TestsRunner.java
@@ -1,9 +1,15 @@
 package examples.tests;
 
 import com.intuit.karate.junit5.Karate;
+import org.junit.jupiter.api.BeforeAll;
 
 class TestsRunner {
-    
+
+    @BeforeAll
+    public static void beforeAll(){
+        System.setProperty("web.url.base","https://www.google.com");
+    }
+
     @Karate.Test
     Karate testUsers() {
         return Karate.run("tests").relativeTo(getClass());
